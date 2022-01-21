60a7741f03584cda28166272904bf3db06edc7e4
Sudharsun <psu.fabulo@gmail.com>
2022-01-21 16:49:03 +0530

    starting card components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Movie from "./components/Movie";
 
 function App() {
+	// const featured = `https://api.themoviedb.org/3/discover/movie?sort_by=popularty.desc&${process.env.REACT_APP_API_KEY}`;
+	// console.log(featured);
+	const movies = [1, 2, 3];
 	return (
 		<div>
-			<p>Movie info cards!</p>
+			{movies.map((movie) => (
+				<Movie />
+			))}
 		</div>
 	);
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Movie = () => {
+	return <div>A movie component</div>;
+};
+
+export default Movie;
