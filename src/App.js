db15eebca6fd1e2c823e87ebb03620698149bf1b
Sudharsun <psu.fabulo@gmail.com>
2022-01-21 17:16:04 +0530

    card components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Movie from "./components/Movie";
 
+const featuredMovies = `https://api.themoviedb.org/3/discover/movie?sort_by=popularty.desc&api_key=${process.env.REACT_APP_API_KEY}`;
+const searchMovies = `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.REACT_APP_API_KEY}&query=`;
+
 function App() {
-	// const featured = `https://api.themoviedb.org/3/discover/movie?sort_by=popularty.desc&${process.env.REACT_APP_API_KEY}`;
-	// console.log(featured);
-	const movies = [1, 2, 3];
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		fetch(featuredMovies)
+			.then((res) => res.json())
+			.then((data) => {
+				console.log(data);
+				setMovies(data.results);
+			});
+	}, []);
 	return (
 		<div>
-			{movies.map((movie) => (
-				<Movie />
-			))}
+			{movies.length > 0 &&
+				movies.map((movie) => <Movie key={movie.id} {...movie} />)}
 		</div>
 	);
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Movie = () => {
-	return <div>A movie component</div>;
+const moviePoster = "https://image.tmdb.org/t/p/w1280";
+
+const Movie = ({ title, poster_path, overview, vote_average }) => {
+	return (
+		<div className="movie">
+			<img src={moviePoster + poster_path} alt={title} />
+
+			<div className="movie-info">
+				<h3>{title}</h3>
+				<span>{vote_average}</span>
+			</div>
+		</div>
+	);
 };
 
 export default Movie;
